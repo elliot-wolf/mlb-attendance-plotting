396756d4a80d48b88905710dd8eb657449500338
elliot-wolf <elliot.r.wolf10@gmail.com>
2025-04-08 15:01:20 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,16 @@
+from setuptools import setup, find_packages
 
+setup(
+    name='mlbattendanceplotter',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'matplotlib',
+        'seaborn',
+        'plotly',
+    ],
+    include_package_data=True,
+    description='A plotting package for viewing MLB Stadium Attendance Trends (2012-2019)',
+    author='Elliot Wolf',
+)
